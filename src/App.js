4ea19173835ce8071b27eea8ec5d39b5b31e367a
Shubham <shubhamawasu@gmail.com>
2021-09-28 23:32:32 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Greet from './Component/p1';
+import G from './Component/p3';
+import Hii from './Component/p2';
+
 function App() {
   return (
     <div className="App">
       Hellow World
     <Greet/>
+    <G/>
+    <Hii/>
     </div>
   );
 }
--- src/Component/p1.js
+++ src/Component/p1.js
@@ -1,6 +1,6 @@
 import React from 'react'
 const Greet=()=>
 {
-return <h1> i am p1 p1</h1>
+return <h1> i am p1 p1cd </h1>
 }
 export default Greet
