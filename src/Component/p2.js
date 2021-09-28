f13efe0bd0e389f0905c1396489274b094c3acd1
Shubham <shubhamawasu@gmail.com>
2021-09-28 23:36:37 +0530

    three

---
+++ src/Component/p2.js
@@ -0,0 +1,7 @@
+import React ,{Component} from 'react'
+class Hii extends Component{
+render(){
+return <h1> hii i am class componanat</h1>
+}
+}
+export default Hii
---
+++ src/Component/p3.js
@@ -0,0 +1,6 @@
+import React from 'react'
+const G=()=>
+{
+return <h1> i am shubham </h1>
+}
+export default G
